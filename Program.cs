a837943f81208a14ef725fcf2a777ddd1bf37b86
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-09-20 08:55:46 +0530

    day1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConAppParameterizedConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student(12, "Sam",
+                new DateTime(day: 12, month: 09, year: 2010));
+            Student stud2 = new Student(11, "Deep", new DateTime(day: 09, month: 11, year: 2009));
+            Console.WriteLine("*** Student One ***");
+            stud1.Display();
+            Console.WriteLine("*** Student Two ***");
+            stud2.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConAppParameterizedConstructor
+{
+    public class Student
+    {
+        readonly int id;
+        readonly string name;
+        readonly DateTime dob;
+        public Student(int id, string name, DateTime dob)
+        {
+            Console.WriteLine("Parameterized Constructor of Student");
+            this.id = id;
+            this.name = name;
+            this.dob = dob;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Student Details as follows !!!");
+            Console.WriteLine("ID :->" + id + "\t Name:-> " + name + "\tDate of Birth :->" + dob);
+        }
+    }
+}
